5bc7645df378d152bd064bc439547f8db884fa64
Karla CRZ <lar.crz.kl@gmail.com>
2025-03-24 18:14:21 -0600

    Mi primer código :sparkles:

---
+++ L1-Estructuras_de_Control/main.js
@@ -0,0 +1,27 @@
+//2do Paso: Trabajar la lógica del programa
+
+//1er ejercicio: Este ejercicio trata de escribir un programa donde se guarde la edad en una variable
+//para que se pueda saber si la persona o usuario es mayor o menor de edad.
+
+//Para tomar como referencia, está es la "anatomía" de la estructura de control [If ... else]:
+
+// if (){
+
+//}else {
+
+//}
+
+///SIGNO: >= (Mayor o igual que)
+
+/// 1er. punto: Declarar una variable.
+let age = 20; // Nunca olvidar agreagar (;) al final de la variable.
+
+// 2do punto: Implementar una condicional o estructura de control [if ... else]
+
+if (age >= 18) {
+    console.log("ERES MAYOR DE EDAD")
+}else {
+    console.log("ERES MENOR DE EDAD")
+}
+
+
