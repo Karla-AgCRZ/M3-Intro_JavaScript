414004d4a1e2211fe938030864232b7e68b0ff4c
Karla CRZ <lar.crz.kl@gmail.com>
2025-03-25 20:26:58 -0600

    :sparkles: Realizando ejercicio 1: Ciclo while

---
+++ L2-Ciclos/while.js
@@ -0,0 +1,14 @@
+// 2do paso: Iniciar con la lógica del código
+
+// 1er ejercicio: Se requiere crear un programa que imprima (enliste) los números del 0 al 10 en la consola.
+
+//1er punto | Inicializamos la variable index en 0 
+let index = 0;
+while(index < 11){
+    //Se determina el bloque de código
+    console.log(index);
+    // Es necesario hacer que el ciclo itere (se repita) nuevamente
+    // por lo cual se necesita una especie de contador en el index
+    // index = index + 1 | <----- primer forma válida
+    index++; //Esto hace que se sume 1 a index
+}
