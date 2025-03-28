2da5f9a64dad43e7d9d6dd3eb7b84939100fee9f
Karla CRZ <lar.crz.kl@gmail.com>
2025-03-27 21:35:48 -0600

    :tada: Ejercicio en clase: L3 - Funciones. Por fin, comienzo a comprender la lógica de JavaScript :tada:

---
+++ L3-Funciones/index.js
@@ -0,0 +1,18 @@
+
+
+function getGreeting(){
+    console.log("HOLA MUNDO! ")
+}
+getGreeting()
+
+
+function getSuma(num1, num2){
+    return num1 + num2
+}
+console.log("La suma es: " + getSuma(2,2))
+
+// El signo (;) es para declarar que un código termino de ser escrito y continua en
+// la siguiente línea, pero JavaScript al ser débilmente tipado no es tan estricto en ello y continua sin necesidad de ello. 
+
+// 3) Función con párametros y donde JavaScript aplique Hoisting
+// el sensei lo subirá la répositorio. 
