42e74defdbf01d2c6d6047f8dde83b7413c4ed7b
Karla CRZ <190561543+Karla-AgCRZ@users.noreply.github.com>
2025-04-14 04:31:24 -0600

    :zap: Continuando con el desarrollo de la práctica de Ciclos y modificando el index.html general del repositorio para comprobar el funcionamiento de mis prácticas

--- L2-Ciclos/while.js
+++ L2-Ciclos/while.js
@@ -1,16 +1,15 @@
-// 2do paso: Iniciar con la lógica del código
+// 2DO PASO | INICIAR A DESARROLLAR LA LÓGICA 
 
-// 1er ejercicio: Se requiere crear un programa que imprima (enliste) los números del 0 al 10 en la consola.
+// 1er Ejercicio: Se requiere crear un programa que imprima (enliste) los números del 0 al 10 en la consola.
 
-//1er punto | Inicializamos la variable index en 0 
+//1er PUNTO | Inicializamos la variable index en 0. 
+//Se determina el bloque de código.
+// Es necesario hacer que el ciclo itere (se repita) nuevamente
+// por lo cual se necesita una especie de contador en el index.
+// index = index + 1 | <----- También es una forma válida.
+
 let index = 0;
-while(index < 11){
-    //Se determina el bloque de código
+while(index < 11) {
     console.log(index);
-    // Es necesario hacer que el ciclo itere (se repita) nuevamente
-    // por lo cual se necesita una especie de contador en el index
-    // index = index + 1 | <----- primer forma válida
-    index++; //Esto hace que se sume 1 a index
+    index++; //Los símbolos ++: Hacen que se sume 1 a index.
 }
-
-//
