341b697869f8333e7087a0b7d94394a7ba458db7
Karla CRZ <190561543+Karla-AgCRZ@users.noreply.github.com>
2025-04-14 08:44:21 -0600

    :sparkles: Iniciando con el ejercicio de práctica de CAMPUS para el entendimiento de los arrays y reforzar el aprendizaje de los ciclos for y while.

--- L2-Ciclos/array.js
+++ L2-Ciclos/array.js
@@ -1,3 +1,10 @@
 //2DO PASO | DESARROLLAR LA LÓGICA
 
-//Ejercicio de práctica de CAMPUS: 
+//Ejercicio de práctica de CAMPUS: Clasificación de Frutas
+//Objetivo: Crear un programa que clasifique las frutas según su tipo y cuente cuántas existen de cada tipo.
+//1er PUNTO | Declarar un 'arreglo o array' llamado 'frutas' con varios tipos de frutas.
+//2do PUNTO | Crear un objeto para almacenar la cantidad de cada tipo de fruta.
+//3er PUNTO | Usar un ciclo 'for o while' para recorrer el arreglo y contar las frutas.
+//4to PUNTO | Imprimir en la consola la cantidad de cada tipo de fruta.
+//5to PUNTO | Tratar de implementar la solución con los dos ciclos 'for y while'.
+
