1bcc4d1149da32f2ad6eec2609ea5c950159a44b
Karla CRZ <190561543+Karla-AgCRZ@users.noreply.github.com>
2025-04-21 18:25:01 -0600

    :zap: Continuando con el ejercicio de práctica para el desarrollo de una simulación de interacción con un cajero automático

--- L3-Funciones/cajero.js
+++ L3-Funciones/cajero.js
@@ -1,26 +1,26 @@
-//2DP PASO: DESARROLLANDO LA LÓGICA
+//2DO PASO: DESARROLLANDO LA LÓGICA
 
 //El saldo inicial de la cuenta es de $1000.
-//● El usuario puede ingresar un monto a retirar.
-//● Se verifica si el monto solicitado es menor o igual al saldo disponible.
-//● Si el monto es válido, se realiza la transacción y se actualiza el saldo.
-//● Se informa al usuario del nuevo saldo disponible.
-//● Si el monto es mayor al saldo disponible, se informa al usuario que la operación no puede realizarse por falta de fondos.
+//El usuario puede ingresar un monto a retirar.
+//Se verifica si el monto solicitado es menor o igual al saldo disponible.
+//Si el monto es válido, se realiza la transacción y se actualiza el saldo.
+//Se informa al usuario del nuevo saldo disponible.
+//Si el monto es mayor al saldo disponible, se informa al usuario que la operación no puede realizarse por falta de fondos.
 
 
-//* 1) Declarar variables
+// 1) Declarar variables
 let saldo = 1000; // Saldo inicial de la cuenta
 let historial = [];  // Historial de transacciones de la cuenta y lo guardo en una lista
 let intentosFallidos = 0; // Intentos fallidos de retiros
 
 
-//* 2) Función para consultar saldo
+// 2) Función para consultar saldo
 function getSaldo(){
     console.log(`Tu saldo actual es de: ${saldo}`); // Mensaje al programador
     alert(`Tu saldo actual es de: ${saldo}`); // Mensaje al usuario
 };
 
-//* 3) Función para realizar retiro
+// 3) Función para realizar retiro
 function retirarSaldo(){
     if(intentosFallidos >= 3){
         alert (`Has excedido el número máximo de intentos fallidos. Tu cuenta ha sido bloqueada.`);
